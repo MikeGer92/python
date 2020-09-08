f5978eb6e6d910fd3119d31f756a7748229e0b33
MikeMan <mikeman@inbox.ru>
2020-09-08 15:55:54 +0300

    create hW1 and hW2

--- hW1_task1.3.py
+++
@@ -1,8 +0,0 @@
-num = input('Введите число от 1 до 10: _')
-full_num = int(num) + int(num + num) + int(num + num + num)
-print(full_num)
-
-
-num2 = int(input('Введите число от 1 до 10: _'))
-full_num2 = num2 + num2 * 11 + num2 * 111
-print(full_num2)
--- hW1_task1.4.py
+++
@@ -1,8 +0,0 @@
-num = int(input('Введите число больше 0: _'))
-last_fig = num % 10
-new_num = num // 10
-while new_num > 0:
-    if new_num % 10 > last_fig:
-        last_fig = new_num % 10
-    new_num = new_num // 10
-print(last_fig)
