9e49676005e93ffbddde76ad5f59d1801ab8b94c
MikeGer <mikeman@inbox.ru>
2020-09-08 19:04:05 +0300

    finish version after chek

--- hW1/hW1_task1.2.py
+++ hW1/hW1_task1.2.py
@@ -1,8 +1,12 @@
 time = int(input('Введите время в секундах: _'))
 hours = time // 3600
-if hours < 10:
-    hours = str(str(0) + str(hours))
 minute = (time - (int(hours) * 3600)) // 60
 sec = ((time % 3600) - (minute * 60)) % 60
+if hours < 10:
+    hours = str(str(0) + str(hours))
+if minute < 10:
+    minute = str(str(0) + str(minute))
+if sec < 10:
+    sec = str(str(0) + str(sec))
 
 print(f'{time} секунд - это {hours} : {minute} : {sec}')
